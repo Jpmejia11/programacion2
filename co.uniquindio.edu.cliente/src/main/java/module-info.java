b3ab7605d52fc7483cc7af7f3a1b0ac6f60108da
Jpmejia11 <juanp.mejiab@uqvirtual.edu.co>
2024-03-04 22:21:15 -0500

    Eliminar repositorio app cliente

--- co.uniquindio.edu.cliente/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.cliente.clienteapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.cliente.clienteapp to javafx.fxml;
-    exports co.edu.uniquindio.cliente.clienteapp;
-}
